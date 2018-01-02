6bddc821ca26a1cbe08fa556df213168e9d95d4a
streq <santirequena@hotmail.com>
2018-01-01 23:00:59 -0300

    agrego archivo Load.js para cargar dinamicamente

---
+++ Load.js
@@ -0,0 +1,33 @@
+function loadSrc(srcs){
+	srcs.forEach(
+		function(src){
+			var script = document.createElement("script");
+			script.src=src;
+			script.async=false;
+			document.head.appendChild(script);
+		}
+	);
+	
+}
+
+function loadGame(prefix){
+	var dependencies = [
+		"Collision.js",
+		"View.js",
+		"Vec2f.js",
+		"MathUtils.js",
+		"Globals.js",
+		"Input.js",
+		"Wall.js",
+		"Smoke.js",
+		"Player.js",
+		"Game.js",
+		"Loop.js"
+	].map(
+		function(src){
+			return prefix+src;
+		}
+	);
+	
+	loadSrc(dependencies);
+}
